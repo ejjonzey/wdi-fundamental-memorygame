ff2b6b61704db95a1ae1f0e671e47f825398ee82
ejjonzey <ejjonzey1982@gmail.com>
2017-06-20 15:04:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var cards = [
 {
-rank: "Queen",
-suit: "Hearts",
-cardImage: "images/queen-of-hearts.png"	
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"	
 },
 {
-rank: "Queen",
-suit: "Diamonds",
-cardImage: "images/queen-of-diamonds.png"
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: "King",
@@ -20,25 +20,21 @@ cardImage: "images/queen-of-diamonds.png"
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
-var cardsInPlay = [],
-cardsInPlay.push(cards[cardId]).rank;
-var checkForMatch = function(); {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
-	}
-};
-var flipCard = function[cardId].rank {
-console.log("user flipped " + cards[cardId]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+var cardsInPlay = [];
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardInPlay[1]) {
 		alert("You found a match!");
-	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again");
+	} else {
+		alert("Sorry, try again.");
 	}
 }
-checkForMatch();
+var flipCard = function(cardId){
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+	cardsInPlay = [];
 };
 flipCard(0);
-flipCard(1);
+flipCard(2);
