b2bdfc6a189a91f2e4c84b4666c9f60584fc35d0
ejjonzey <ejjonzey1982@gmail.com>
2017-06-20 14:20:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-var cards = ["King", "King", "Queen", "Queen"];
-var cardsInPlay = [];
-var checkForMatch = function() {
+var cards = [
+{
+rank: "Queen",
+suit: "Hearts",
+cardImage: "images/queen-of-hearts.png"	
+},
+{
+rank: "Queen",
+suit: "Diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+var cardsInPlay = [],
+cardsInPlay.push(cards[cardId]).rank;
+var checkForMatch = function(); {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again.");
 	}
 };
-var flipCard = function(cardId) {
+var flipCard = function[cardId].rank {
 console.log("user flipped " + cards[cardId]);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -16,7 +38,7 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again");
 	}
 }
+checkForMatch();
 };
 flipCard(0);
 flipCard(1);
-checkForMatch();
