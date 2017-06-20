73aabbed045433be6d3085a4944f35fec02109c1
ejjonzey <ejjonzey1982@gmail.com>
2017-06-20 13:30:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-console.log("Up and running");
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "Queen";
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+var cards = ["King", "King", "Queen", "Queen"];
+var cardsInPlay = [];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+var flipCard = function(cardId) {
+console.log("user flipped " + cards[cardId]);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again");
+	}
+}
+};
+flipCard(0);
+flipCard(1);
+checkForMatch();
