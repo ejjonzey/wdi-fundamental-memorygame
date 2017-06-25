362d25092948e13d834fd7c25b2ad861a349295b
ejjonzey <ejjonzey1982@gmail.com>
2017-06-25 15:36:58 -0400

    Add files via upload
    add game logic

--- main.js
+++ main.js
@@ -21,14 +21,16 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
-var checkForMatch = function () {
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardInPlay[1]) {
+var checkForMatch = function() {
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute("data-id");
 	console.log("user flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -36,5 +38,13 @@ var flipCard = function(cardId){
 	checkForMatch();
 	cardsInPlay = [];
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game board").appendChild(cardElement);
+  }
+};
+createBoard();
